15628496132e482bda875546de57b335eb51b9cb
Gabriel Bernardi <50278200+gabrielrbernardi@users.noreply.github.com>
2019-09-29 14:21:13 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -33,9 +33,30 @@ function data() {
 /* Set the width of the side navigation to 250px */
 function openNav() {
     document.getElementById("mySidenav").style.width = "200px";
+    // Comandos para pegar a data e hora e colocar no html
+    var d = new Date(),
+        dia, mes, ano, hora, minuto;
+    dia = d.getDate();
+    mes = d.getMonth() + 1;
+    ano = d.getFullYear();
+    document.getElementById("date").innerHTML = dia + '/' + mes + '/' + ano; //inserindo a data no HTML
+    hora = d.getHours();
+    minuto = d.getMinutes();
+    if (minuto >= 0 && minuto < 10) {
+        minuto = '0' + minuto;
+    }
+    if (hora >= 0 && hora < 10) {
+        hora = '0' + hora;
+    }
+    document.getElementById("time").innerHTML = hora + ':' + minuto; //inserindo o horario no HTML
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+}
+
+function login() {
+    // var user = window.confirm("Usuario");
+    document.getElementById("teste").innerHTML = "ola";
 }

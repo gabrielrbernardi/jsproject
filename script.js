949bacc51e44349944a5e061121127362a5b60d0
Gabriel Bernardi <50278200+gabrielrbernardi@users.noreply.github.com>
2019-10-01 23:32:32 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var correctUser = "GABRIEL"
+var correctPwd = "TESTE"
+
 var str = "A data e hora serão alterados aqui";
 
 var x = texto();
@@ -34,6 +37,8 @@ function data() {
 function openNav() {
     document.getElementById("mySidenav").style.width = "200px";
     // Comandos para pegar a data e hora e colocar no html
+    document.getElementById("mySidenav").style.width = "200px";
+    document.getElementById("main").style.marginRight = "200px";
     var d = new Date(),
         dia, mes, ano, hora, minuto;
     dia = d.getDate();
@@ -54,9 +59,43 @@ function openNav() {
 /* Set the width of the side navigation to 0 */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginRight = "0";
 }
 
-function login() {
+function loginLinks() {
     // var user = window.confirm("Usuario");
-    document.getElementById("teste").innerHTML = "ola";
+    var user = window.prompt("Usuário");
+    if (user) {
+        user = user.toUpperCase();
+        var pwd = window.prompt("Senha");
+        if (pwd) {
+            pwd = pwd.toUpperCase();
+            if (user == correctUser && pwd == correctPwd) {
+                location.assign("https://www.google.com");
+
+            } else {
+                deniedAccess();
+            }
+        } else {
+            deniedAccess();
+        }
+    } else {
+        deniedAccess();
+    }
+}
+
+function errorLogin() {
+    location.assign("error.html");
+}
+
+var varTime1, varTime2;
+
+function deniedAccess() {
+    alert("Acesso negado");
+    document.getElementById("teste").innerHTML = "ACESSO NEGADO";
+    varTime1 = setInterval(errorLogin, 750);
+    varTime2 = setInterval(closeNav, 500);
+    // window.history.back();
+
 }

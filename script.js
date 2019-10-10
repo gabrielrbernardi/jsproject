68359ac2c707deea5bc8f46d42b9aa33a55bf9cd
Gabriel Bernardi <50278200+gabrielrbernardi@users.noreply.github.com>
2019-10-09 23:16:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ function loginLinks() {
 }
 
 function errorLogin() {
-    location.assign("/error.html");
+    location.assign("./error.html");
 }
 
 var varTime1, varTime2;
@@ -96,7 +96,7 @@ var varTime1, varTime2;
 function deniedAccess() {
     alert("Acesso negado");
     document.getElementById("teste").innerHTML = "ACESSO NEGADO";
-    varTime1 = setInterval(errorLogin, 1);
+    varTime1 = setInterval(errorLogin, 50);
     varTime2 = setInterval(closeNav, 500);
     // window.history.back();
 
